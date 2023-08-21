7f2e45291eaa2c0e600c1514669fd39b49b89aba
dminh31 <vbducminh3101@gmail.com>
2023-08-21 10:26:42 +0700

    btapss8

---
+++ index.js
@@ -0,0 +1,25 @@
+console.log("welcome to Rikkei Academy");
+prompt("mời bạn nhập số tuổi của mình")
+document.write("Quyết tâm học lập trình javascript")
+alert("Chào mừng bạn đến với học việc Rikkei Academy")
+confirm("bạn đã đủ 18 tuổi chưa!")
+
+let ly = +prompt("điểm lí");
+let hoa = +prompt("điểm hóa");
+let sinh = +prompt("điểm sinh");
+let tong = ly + hoa + sinh
+let tb = tong / 3;
+console.log("điểm môn ly là math", ly);
+console.log(`điểm môn sinh la ${math}`);
+console.log(tong);
+console.log(tb);
+
+
+let C = +prompt("độ C");
+let F = (9 * C) + 32 / 9;
+console.log(F);
+
+let radius = +prompt("nhập bán kính")
+const PI = 3.14;
+let result = radius * radius * PI
+console.log(`diện tích hình tròn có bán kính ${radius} ; ${result}`);
